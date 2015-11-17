2ed2c32a7c97737042781865e56f89cce6b8afdb
Timothy Chung <timchunght@gmail.com>
2015-11-16 21:26:14 -0500

    create endpoint basically functional

--- handlers.go
+++ handlers.go
@@ -58,25 +58,17 @@ curl -H "Content-Type: application/json" -d '{"name":"New Item"}' http://localho
 func ItemCreate(w http.ResponseWriter, r *http.Request) {
 	var item Item
 	fmt.Println(item)
-	// body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// if err := r.Body.Close(); err != nil {
-	// 	panic(err)
-	// }
-	// if err := json.Unmarshal(body, &item); err != nil {
-	// 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	// 	w.WriteHeader(422) // unprocessable entity
-	// 	if err := json.NewEncoder(w).Encode(err); err != nil {
-	// 		panic(err)
-	// 	}
-	// }
+	
+	name := r.URL.Query().Get("name")
+	url := r.URL.Query().Get("url")
+	// fmt.Println(string(name))
+	item.Name = string(name)
+	item.Url = string(url)
 
-	// t := RepoCreateItem(item)
-	// w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	// w.WriteHeader(http.StatusCreated)
-	// if err := json.NewEncoder(w).Encode(t); err != nil {
-	// 	panic(err)
-	// }
+	t := RepoCreateItem(item)
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusCreated)
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		panic(err)
+	}
 }
